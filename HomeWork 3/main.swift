76852206f79e1eec8b951fed73d55990bfc55894
Bak8 <ataibegaliev@gmail.com>
2022-09-18 23:24:16 +0600

    wd

--- HomeWork 3/main.swift
+++ HomeWork 3/main.swift
@@ -1,11 +1,36 @@
-//
-//  main.swift
-//  HomeWork 3
-//
-//  Created by Atai Begaliev on 18/9/22.
-//
 
-import Foundation
+print("Please type the city")
 
-print("Hello, World!")
+func newShowString(str:String) {
+    switch str {
+    case "Bishkek" , "bishkek": print("22 Sept - 28°C | 23 Sept - 26°C | 24 Sept - 31°C ")
+    case "Cholpon-Ata", "cholpo-ata": print("22 Sept - 21°C | 23 Sept - 22°C | 24 Sept - 24°C ")
+    case "Talas", "talas": print("22 Sept - 19°C | 23 Sept - 20°C | 24 Sept - 18°C ")
+    case "Osh" , "osh": print("22 Sept - 22°C | 23 Sept - 23°C | 24 Sept - 19°C ")
+    case "Jalal-Abad" , "jalal-abad": print("22 Sept - 32°C | 23 Sept - 31°C | 24 Sept - 31°C ")
+    case "Naryn" , "naryn": print("22 Sept - 19°C | 23 Sept - 18°C | 24 Sept - 21°C ")
+    case "Batken" , "batken": print("22 Sept - 25°C | 23 Sept - 26°C | 24 Sept - 29°C ")
+    default: print("Please type correct city")
+    }
+}
+let str = readLine()
+newShowString(str: String(str!))
 
+
+//(Бишкек, Чолпон-Ата, Талас, Ош, Джалал-Абад, Нарын, Баткен)
+
+//newShowString(str: "bishkek")
+
+
+//func showString(num: Int) {
+//    if num % 2 == 0 && num<10{
+//        print("EVEN")
+//    }else if num % 2 == 1 && num<10  {
+//        print("ODD")
+//    }else{
+//        print("Число больше 10")
+//    }
+//}
+//
+//let num = readLine()
+//showString(num: Int(num!)! )
